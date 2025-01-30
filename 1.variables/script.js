6b884d0ef633a624e6f9d0c890669e1b1da956d9
Ylana Oliveira <ylanalmo@gmail.com>
2025-01-30 11:57:32 -0300

    file name change

---
+++ 1.variables/script.js
@@ -0,0 +1,25 @@
+console.log('Hello, world!');
+
+//const name = 'Ylana'; //não permite reasignar valores
+//name = 'Amora'; //erro
+
+//let age = 31; //permite reasignar valores
+//age = 32;
+
+//exercise
+let firstName = 'Ylana';
+let age = 31;
+
+let comidaFavorita;
+comidaFavorita = 'Estrogonofe de Grão-de-Bico';
+
+let preco,
+quantidade,
+totalCompra,
+totalVenda,
+lucro;
+
+console.log(firstName, age, comidaFavorita);
+
+//descobrir qual tipo de dado
+console.log(typeof firstName);
