8c513c47f9c4fc1b97c4a9d25177869dc2eddabf
Ylana Oliveira <ylanalmo@gmail.com>
2025-02-16 18:53:56 -0300

    complete

--- 20.object-array/script.js
+++ 20.object-array/script.js
@@ -20,13 +20,13 @@ dados[1][2].cor; // azul
 // Construção de Arrays
 // Toda array herda os métodos e propriedades do protótipo do construtor Array.
 
-const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
-const carros = new Array('Corola', 'Mustang', 'Honda');
+const instrumentos1 = ['Guitarra', 'Baixo', 'Violão'];
+const cars = new Array('Corola', 'Mustang', 'Honda');
 
-carros[1] // Mustang
-carros[2] = 'Ferrari';
-carros[10] = 'Parati';
-carros.length; // 11
+cars[1] // Mustang
+cars[2] = 'Ferrari';
+cars[10] = 'Parati';
+cars.length; // 11
 
 
     // Os valores das array's não são estáticos
@@ -40,14 +40,14 @@ carros.length; // 11
 let li = document.querySelectorAll('li'); // NodeList
 li = Array.from(li); // Array
 
-const carros = {
+const carros1 = {
   0: 'Fiat',
   1: 'Honda',
   2: 'Ford',
   length: 4,
 }
 
-const carrosArray = Array.from(carros);
+const carrosArray = Array.from(carros1);
 
 
 
@@ -56,11 +56,11 @@ const carrosArray = Array.from(carros);
 // Array.isArray()
 // Verifica se o valor passado é uma array e retorna um valor booleano.
 
-let li = document.querySelectorAll('li'); // NodeList
-Array.isArray(li); // false
+let li2 = document.querySelectorAll('li'); // NodeList
+Array.isArray(li2); // false
 
-li = Array.from(li); // Array
-Array.isArray(li); // true
+li2 = Array.from(li2); // Array
+Array.isArray(li2); // true
 
 
 
@@ -97,12 +97,12 @@ frutas[2].length; // 2
 // Métodos Modificadores [].sort()
 // Os próximos métodos que vamos falar sobre, são métodos modificadores (mutator methods). Além de retornarem um valor, eles modificam a array original. [].sort() organiza a pelo unicode.
 
-const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
-instrumentos.sort();
-instrumentos; // ['Baixo', 'Guitarra', Violão]
+const instrumentos2 = ['Guitarra', 'Baixo', 'Violão'];
+instrumentos2.sort();
+instrumentos2; // ['Baixo', 'Guitarra', Violão]
 
 const idades = [32,21,33,43,1,12,8];
-idades.sort();
+idades.sort(); //não funciona com numeros
 idades; // [1, 12, 21, 32, 33, 43, 8]
 
 
@@ -112,12 +112,12 @@ idades; // [1, 12, 21, 32, 33, 43, 8]
 // [].unshift() e [].push()
 // [].unshift() adiciona elementos ao início da array e retorna o length da mesma. [].push() adiciona elementos ao final da array e retorna o length da mesma.
 
-const carros = ['Ford', 'Fiat', 'VW'];
-carros.unshift('Honda', 'Kia'); // 5
-carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
+const carros0 = ['Ford', 'Fiat', 'VW'];
+carros0.unshift('Honda', 'Kia'); // 5
+carros0; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
 
-carros.push('Ferrari'); // 6
-carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
+carros0.push('Ferrari'); // 6
+carros0; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
 
 
 
@@ -127,12 +127,12 @@ carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
 // [].shift() e [].pop()
 // [].shift() remove o primeiro elemento da array e retorna o mesmo. [].pop() remove o último elemento da array e retorna o mesmo.
 
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-const primeiroCarro = carros.shift(); // 'Ford'
-carros; // ['Fiat', 'VW', 'Honda'];
+const carros2 = ['Ford', 'Fiat', 'VW', 'Honda'];
+const primeiroCarro = carros2.shift(); // 'Ford'
+carros2; // ['Fiat', 'VW', 'Honda'];
 
-const ultimoCarro = carros.pop(); // 'Honda'
-carros; // ['Fiat', 'VW'];
+const ultimoCarro = carros2.pop(); // 'Honda'
+carros2; // ['Fiat', 'VW'];
 
 
 
@@ -141,8 +141,8 @@ carros; // ['Fiat', 'VW'];
 // [].reverse()
 // [].reverse() inverte os itens da array e retorna a nova array.
 
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
+const carros3 = ['Ford', 'Fiat', 'VW', 'Honda'];
+carros3.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
 
 
 
@@ -151,12 +151,12 @@ carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
 // [].splice()
 // [].splice(index, remover, item1, item2, ...) adiciona valores na array a partir do index. Remove a quantidade de itens que for passada no segundo parâmetro (retorna esses itens).
 
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-carros.splice(1, 0, 'Kia', 'Mustang'); // []
-carros; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
+const carros4 = ['Ford', 'Fiat', 'VW', 'Honda'];
+carros4.splice(1, 0, 'Kia', 'Mustang'); // []
+carros4; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
 
-carros.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
-carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
+carros4.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
+carros4; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
 
 
 
@@ -188,7 +188,7 @@ carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
 // ['Item1', 'Banana', 'Banana', 'Item4']
 
 
-
+//--------------------------------------------------------
 
 
 // Métodos de Acesso [].concat()
@@ -226,10 +226,10 @@ linguagens.lastIndexOf('js'); // 5
 
 // [].join(separador) junta todos os valores da array e retorna uma string com eles. Se você passar um valor como parâmetro, este será utilizado durante a junção de cada item da array.
 
-const linguagens = ['html', 'css', 'js', 'php', 'python'];
-linguagens.join(); // 'html,css,js,php,python'
-linguagens.join(' '); // 'html css js php python'
-linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
+const linguagens2 = ['html', 'css', 'js', 'php', 'python'];
+linguagens2.join(); // 'html,css,js,php,python'
+linguagens2.join(' '); // 'html css js php python'
+linguagens2.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
 
 let htmlString = '<h2>Título Principal</h2>'
 htmlString = htmlString.split('h2');
@@ -245,30 +245,50 @@ htmlString = htmlString.join('h1');
 // [].slice()
 // [].slice(inicio, final) retorna os itens da array começando pelo início e indo até o valor de final.
 
-const linguagens = ['html', 'css', 'js', 'php', 'python'];
-linguagens.slice(3); // ['php', 'python']
-linguagens.slice(1, 4); // ['css', 'js', 'php']
+const linguagens3 = ['html', 'css', 'js', 'php', 'python'];
+linguagens3.slice(3); // ['php', 'python']
+linguagens3.slice(1, 4); // ['css', 'js', 'php']
 
-const cloneLinguagens = linguagens.slice();
-
-
+const cloneLinguagens = linguagens3.slice();
 
 
+
+//-------------------------------------------------------
 
 
 // Exercícios
 
 const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
 // Remova o primeiro valor de comidas e coloque em uma variável
+const first = comidas.shift();
+console.log(first);
+
 // Remova o último valor de comidas e coloque em uma variável
+const last = comidas.pop();
+console.log(last);
+
 // Adicione 'Arroz' ao final da array
+comidas.push('Arroz');
+console.log(comidas);
+
 // Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift('Peixe', 'Batata');
+console.log(comidas);
 
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
+estudantes.sort();
+console.log(estudantes);
+
 // Inverta a ordem dos estudantes
+estudantes.reverse();
+console.log(estudantes);
+
 // Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'));
+
 // Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'));
 
 let html = `<section>
               <div>Sobre</div>
@@ -277,7 +297,13 @@ let html = `<section>
             </section>`
 // Substitua section por ul e div com li,
 // utilizando split e join
+const newHtml = html.split('section').join('ul').split('div').join('li');
+console.log(newHtml)
 
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+const cloneCarros = carros.slice();
+cloneCarros.pop();
+console.log(carros);
+console.log(cloneCarros);
