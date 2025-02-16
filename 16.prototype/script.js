5202bcb847944ee5c01cd21676435856ccd47f49
Ylana Oliveira <ylanalmo@gmail.com>
2025-02-15 22:52:57 -0300

    complete

--- 16.prototype/script.js
+++ 16.prototype/script.js
@@ -1,197 +1,143 @@
-// //OBJETOS - Constructor Functions
-// //Objetos
-// //Criar um objeto é simples, basta definirmos uma variável e iniciar a definição do seu valor com chaves {}. Mas e se precisarmos criar um novo objeto, com as mesmas características do anterior? É possível com o Object.create, mas veremos ele mais tarde.
+// //Prototype
+// //A propriedade prototype é um objeto adicionado a uma função quando a mesma é criada.
 
-// const carro = {
-//   marca: 'Marca',
-//   preco: 0,
+// function Pessoa(nome, idade) {
+//   this.nome = nome;
+//   this.idade = idade;
+// }
+// const andre = new Pessoa('André', 28);
+
+// console.log(Pessoa.prototype); // retorna o objeto
+// console.log(andre.prototype); // undefined
+
+// //funcao.prototype
+// //É possível adicionar novas propriedades e métodos ao objeto prototype.
+
+// Pessoa.prototype.andar = function() {
+//   return this.nome + ' andou';
+// }
+// Pessoa.prototype.nadar = function() {
+//   return this.nome + ' nadou';
+// }
+// console.log(Pessoa.prototype); // retorna o objeto
+
+// //Acesso ao Protótipo
+// //O objeto criado utilizando o construtor, possui acesso aos métodos e propriedades do protótipo deste construtor. Lembrando, prototype é uma propriedade de funções apenas.
+
+// const andre = new Pessoa('André', 28);
+
+// andre.nome;
+// andre.idade;
+// andre.andar();
+// andre.nadar();
+
+// //proto
+// //O novo objeto acessa os métodos e propriedades do protótipo através da propriedade __proto__. É papel da engine fazer essa busca, não devemos falar com __proto__ diretamente.
+
+// //Acessam o mesmo método mas __proto__ não terá acesso ao this.nome
+// andre.andar();
+// andre.__proto__.andar();
+
+// //Herança de Protótipo
+// //O objeto possui acesso aos métodos e propriedades do protótipo do construtor responsável por criar este objeto. O objeto abaixo possui acesso a métodos que nunca definimos, mas são herdados do protótipo de Object.
+
+// Object.prototype;
+// andre.toString();
+// andre.isPrototypeOf();
+// andre.valueOf();
+
+// //Construtores Nativos
+// //Objetos, Funções, Números, Strings e outros tipos de dados são criados utilizando construtores. Esses construtores possuem um protótipo com propriedades e métodos, que poderão ser acessadas pelo tipo de dado.
+
+// const pais = 'Brasil';
+// const cidade = new String('Rio');
+
+// pais.charAt(0); // B
+// cidade.charAt(0); // R
+
+// String.prototype;
+
+// //É possível acessar a função do protótipo
+// //É comum, principalmente em códigos mais antigos, o uso direto de funções do protótipo do construtor Array.
+
+// const lista = document.querySelectorAll('li');
+// // Transforma em uma array
+// const listaArray = Array.prototype.slice.call(lista);
+
+// //Existe o método Array.from()
+
+// //Método do Objeto vs Protótipo
+// //Nos objetos nativos existem métodos linkados diretamente ao Objeto e outros linkados ao protótipo.
+
+// Array.prototype.slice.call(lista);
+// Array.from(lista);
+
+// // Retorna uma lista com os métodos / propriedades
+// Object.getOwnPropertyNames(Array);
+// Object.getOwnPropertyNames(Array.prototype);
+
+// //dado.constructor.name, retorna o nome do construtor;
+
+// //Apenas os Métodos do Protótipo são Herdados
+// [1,2,3].slice(); // existe
+// [1,2,3].from(); // não existe
+
+
+
+
+// //Entenda o Que está Sendo Retornado
+// //Os métodos e propriedades acessado com o . são referentes ao tipo de dados que é retornado antes desse .
+
+// const Carro = {
+//   marca: 'Ford',
+//   preco: 2000,
+//   acelerar() {
+//     return true;
+//   }
 // }
 
-// const honda = carro;
-// honda.marca = 'Honda';
-// honda.preco = 4000;
+// Carro // Object
+// Carro.marca // String
+// Carro.preco // Number
+// Carro.acelerar // Function
+// Carro.acelerar() // Boolean
+// Carro.marca.charAt // Function
+// Carro.marca.charAt(0) // String
 
-// const fiat = carro;
-// fiat.marca = 'Fiat';
-// fiat.preco = 3000;
-// //carro, fiat e honda apontam para o mesmo objeto, é errado fazer dessa forma, estou alterando o mesmo objeto várias vezes e no final fica a ultima alteração
-
-
-// //Constructor Functions
-// //Para isso existem as Constructor Functions, ou seja, Funções Construtoras que são responsáveis por construir novos objetos sempre que chamamos a mesma.
-
-//  //Usar Pascal Case, ou seja, começar com letra maiúscula.
-
-// function Carro() {
-//   this.marca = 'Marca';
-//   this.preco = 0;
-// }
-
-// const honda1 = new Carro();
-// honda1.marca = 'Honda';
-// honda1.preco = 4000;
-// const fiat1 = new Carro();
-// fiat1.marca = 'Fiat';
-// fiat1.preco = 3000;
-
-// //new Keyword
-// //A palavra chave new é responsável por criar um novo objeto baseado na função que passarmos a frente dela.
-
-// const honda = new Carro();
-
-// // 1 Cria um novo objeto
-// honda = {};
-
-// // 2 Define o protótipo
-// honda.prototype = Carro.prototype;
-
-// // 3 Aponta a variável this para o objeto
-// this = honda;
-
-// // 4 Executa a função, substituindo this pelo objeto
-// honda.marca = 'Marca';
-// honda.preco = 0;
-
-// // 5 Retorna o novo objeto
-// return honda = {
-//   marca: 'Marca',
-//   preco: 0,
-// }
-
-
-// //Parâmetros e Argumentos
-// //Podemos passar argumentos que serão utilizados no momento da criação do objeto.
-
-// function Carro(marca, preco) {
-//   this.marca = marca;
-//   this.preco = preco;
-// };
-
-// const BYD = new Carro('BYD', 4000);
-// const fiat2 = new Carro('Fiat', 3000);
-
-
-// //this Keyword
-// //O this faz referência ao próprio objeto construído com a Constructor Function.
-// function Carro(marca, precoInicial) {
-//   const taxa = 1.2;
-//   const precoFinal = precoInicial * taxa;
-//   this.marca = marca;
-//   this.preco = precoFinal;
-//   console.log(this);
-// }
-
-// const honda = new Carro('Honda', 2000);
-
-// //Variáveis dentro da Constructor estão "protegidas".
-
-
-
-//Exemplo Real
-//Quando mudamos a propriedade seletor, o objeto Dom irá passar a selecionar o novo seletor em seus métodos.
-
-// const Dom = {
-//   seletor: 'li',
-//   element() {
-//     return document.querySelector(this.seletor);
-//   },
-//   ativo() {
-//     this.element().classList.add('ativo');
-//   },
-// }
-
-// Dom.ativo(); // adiciona ativo ao li
-// Dom.seletor = 'ul';
-// Dom.ativo(); // adiciona ativo ao ul
-
-
-
-//Constructor Function Real
-//Um objeto criado com uma Constructor, não irá influenciar em outro objeto criado com a mesma Constructor.
-
-// function Dom() {
-//   this.seletor = 'li';
-//   const element = document.querySelector(this.seletor);
-//   this.ativo = function() {
-//     element.classList.add('ativo');
-//   };
-// }
-
-// const lista = new Dom();
-// lista.seletor = 'ul';
-// lista.ativo();
-
-// const lastLi = new Dom();
-// lastLi.seletor = 'li:last-child';
-// lastLi.ativo();
-
-// //Lembre-se de usar parâmetros
-// function Dom(seletor) {
-//   const element = document.querySelector(seletor);
-//   this.ativo = function(classe) {
-//     element.classList.add(classe);
-//   };
-// }
-
-// const lista = new Dom('ul');
-// lista.ativo('ativo');
-
-// const lastLi = new Dom('li:last-child');
-// lastLi.ativo('ativo');
+// //Verifique o nome do construtor com .constructor.name
 
 
 
 //Exercícios
-// Transforme o objeto abaixo em uma Constructor Function
-// const pessoa = {
-//   nome: 'Nome pessoa',
-//   idade: 0,
-//   andar() {
-//     console.log(this.nome + ' andou');
-//   }
-// }
-
-function Pessoa(nome, idade) {
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoa(nome, sobrenome, idade) {
   this.nome = nome;
+  this.sobrenome = sobrenome;
   this.idade = idade;
-  this.andar = function () {
-    console.log(`${this.nome} andou`);
-  }
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+  return `${this.nome} ${this.sobrenome}`
 };
+const ylana = new Pessoa('Ylana', 'Leal Melo de Oliveira', 31);
 
-const ylana = new Pessoa('Ylana', 31);
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
 
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
 
-// Crie 3 pessoas, João - 20 anos,
-// Maria - 25 anos, Bruno - 15 anos
-const joao = new Pessoa('João', 20);
-const maria = new Pessoa('Maria', 25);
-const bruno = new Pessoa('Bruno', 15);
+li; //HTMLLIElement
+li.click; //function
+li.innerText; //string
+li.value;//number
+li.hidden;//boolean
+li.offsetLeft;//number
+li.click();//undefined
 
-
-// Crie uma Constructor Function (Dom) para manipulação
-// de listas de elementos do dom. Deve conter as seguintes
-// propriedades e métodos:
-//
-// elements, retorna NodeList com os elementos selecionados
-// addClass(classe), adiciona a classe a todos os elementos
-// removeClass(classe), remove a classe a todos os elementos
-function Dom(seletor) {
-  this.elements = document.querySelectorAll(seletor);
-  this.addClass = function (classe) {
-    this.elements.forEach((element) => {
-      element.classList.add(classe);
-  });
-};
-  this.removeClass = function (classe) {
-    this.elements.forEach((element) => {
-      element.classList.remove(classe);
-  });
-  };
-};
-
-const lista = new Dom('li');
-console.log(lista.elements);
-lista.addClass('ativo');
-lista.addClass('lista');
-lista.removeClass('lista');
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; //string
