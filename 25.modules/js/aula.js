3306a7fcba7ba3111cb7bff16a7ffcb6d33015f5
Ylana Oliveira <ylanalmo@gmail.com>
2025-02-19 14:44:41 -0300

    complete

---
+++ 25.modules/js/aula.js
@@ -0,0 +1,135 @@
+// Efeitos no Dom
+
+// Modules Introdução
+// Módulos
+
+//     Manutenção
+
+//     Dividir o código em diferentes arquivos com funções específicas (módulos) facilita a manutenção.
+
+//     Compartilhamento
+
+//     O compartilhamento de código com outros projetos é facilitado, pois basta você importar um módulo específico.
+
+//     Nativo no ES6+
+
+//     Ferramentas que permitem dividirmos o código em módulos já existem a bastante tempo. Grunt, Gulp, Webpack, Browserify, Parcel e outras. Mas agora os módulos são nativos.
+
+
+
+
+//     Modules ES6
+
+// Basta adicionar type="module" na tag script do HTML. Utilize a palavra chave export na frente do valor que deseja exportar (use default se for único). E import nome from arquivo.js para importar.
+
+<script type="module" src="js/script.js"></script>
+
+
+// arquivo scroll-suave.js
+export default function scrollSuave() {
+  ...
+};
+
+
+// arquivo script.js
+import scrollSuave from './scroll-suave.js';
+
+scrollSuave();
+
+
+//     Geralmente um valor por módulo.
+
+
+
+
+
+
+// Named Exports
+
+// Você pode exportar mais de um valor. Quando for importar utilize as chaves para especificar cada valor. O nome importado deve ser igual ao exportado.
+
+// arquivo scroll.js
+export function scrollSuave() {
+  ...
+};
+export function scrollAnimacao() {
+  ...
+};
+
+
+// arquivo script.js
+import { scrollSuave, scrollAnimacao } from './scroll.js';
+scrollSuave();
+scrollAnimacao();
+
+
+// Importe todos os valores em um objeto
+import * as scroll from './scroll.js';
+scroll.scrollSuave();
+scroll.scrollAnimacao();
+
+
+
+
+
+
+// Valores do Export
+
+// Podemos exportar objetos, funções, classes, números, strings e mais.
+
+// arquivo configuracao.js
+export function scrollSuave() {};
+export const ano = 2000;
+export const obj = {nome: 'Ford'};
+export const str = 'Frase';
+export class Carro {};
+
+
+// arquivo script.js
+import * as conf from './configuracao.js';
+conf.str;
+conf.obj;
+conf.ano;
+
+// Características
+
+//     Strict mode
+
+//     'use strict' por padrão em todos os arquivos.
+
+//     Variáveis ficam no module apenas
+
+//     Não vazam para o escopo globo.
+
+//     This fora de um objeto faz referência a undefined
+
+//     Ao invés de fazer referência ao window.
+
+//     Assíncrono
+
+
+
+
+
+
+
+// use strict
+
+// O modo estrito previne que algumas ações consideradas erros. Basta adicionarmos 'use strict' no topo de um arquivo, que ele entrará neste modo.
+
+'use strict';
+
+nome = 'Ford'; // erro, variável global
+delete Array.prototype; // erro, não deletável
+window.top = 200; // erro, não pode mudar
+const arguments = 3.14; // escrever em palavra reservada
+
+
+//     Por padrão todo module está no modo estrito
+
+
+
+
+// Exercício
+
+// Divida o projeto em diferentes módulos
