5c62fa3146514a95a2d2c2bc267a4b98cb754567
Ylana Oliveira <ylanalmo@gmail.com>
2025-02-19 13:46:25 -0300

    complete

--- 24.efeitos-dom-dataset/script.js
+++ 24.efeitos-dom-dataset/script.js
@@ -1,96 +1,83 @@
-function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+// // Efeitos no Dom
+// // Dataset
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+// // HTMLElement
+// //Todo elemento HTML do DOM herda propriedades e métodos do construtor HTMLElement.
 
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove('ativo');
-      })
-      tabContent[index].classList.add('ativo');
-    };
+// const h1 = document.querySelector('h1');
+// Object.prototype.toString.call(h1); // [object HTMLHeadingElement]
+// // HTMLHeadingElement > HTMLElement > Element > Node > EventTarget > Object
 
-    tabMenu.forEach((item, index) => {
-      item.addEventListener('click', () => {
-        activeTab(index);
-      });
-    });
-  };
-};
+// //Dataset
+// //dataset é uma propriedade de HTMLElement, essa propriedade é um objeto do tipo DOMStringMap. Dentro desse objeto existe uma coleção de chave / valor, com todos os atributos do elemento html que começarem com data-.
 
-initTabNav();
+// {/* <div data-cor="azul" data-width="500">Uma Div</div>
+// <span data-anime="left" data-tempo="2000">Um Span</span> */}
 
-function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const ativoClass = 'ativo'
+// // Ambos os valores selecionam a mesma div acima.
+// let div = document.querySelector('div');
+// div = document.querySelector('[data-cor]');
+// div = document.querySelector('[data-cor="azul"]');
 
-  if(accordionList.length) {
-    accordionList[0].classList.add(ativoClass);
-    accordionList[0].nextElementSibling.classList.add(ativoClass);
+// div.dataset;
+// // DOMStringMap {cor: "azul", width: "500"}
+// div.dataset.cor; // 'azul'
+// div.dataset.width; // '500'
+// div.dataset.tempo = 1000;
+// // DOMStringMap {cor: "azul", width: "500", tempo: "1000"}
 
-    function activeAccordion() {
-      this.classList.toggle(ativoClass);
-      this.nextElementSibling.classList.toggle(ativoClass);
-    };
+// // Data Atributes
+// // Os atributos e valores que começarem com data- poderão ser utilizados como forma de configuração de plugins e interações DOM / JS.
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
-    });
-  };
-};
+// {/* <div data-anima="left" data-tempo="1000">Div 1</div>
+// <div data-anima="right" data-tempo="2000">Div 2</div> */}
 
-initAccordion();
+// const divs = document.querySelectorAll('[data-anima]');
+// divs.forEach((div) => {
+//   div.classList.add(div.dataset.anima);
+// });
 
-function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+// // adiciona em cada div
+// // uma classe com o mesmo nome
+// // que o valor de data
 
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    //outra forma que não é suportada por todos os browser
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+// // Data vs Class
+// // A vantagem de se utilizar data atributes é que torna mais fácil evitarmos conflitos com estilos do CSS. Além de deixar a estrutura da tag mais organizada.
 
-    //forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
-  };
+// // {/* <div data-anima="left" data-tempo="1000">Div 1</div>
+// // <div class="anima-left tempo-1000">Div 2</div> */}
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection)
-  });
-};
+// // Nomenclatura
+// // Por padrão o JavaScript não aceita - (traço) como caracter válido para nomear propriedades. Então qualquer traço será removido e a letra seguinte transformada em maiúscula.
 
-initScrollSuave();
+// <div data-anima-scroll="left">Div 1</div>
 
+// const div = document.querySelector('[data-anima-scroll]');
+// div.dataset;
+// // {animaScroll: 'left'}
+// div.dataset.animaScroll; // left
+// div.dataset.tempoTotal = 1000;
+// // Na div vira data-tempo-total="1000"
 
-function initAnimationScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
+// delete div.dataset.animaScroll; // remove o atributo
 
-  if(sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else
-          section.classList.remove('ativo');
-      });
-    };
+//Exercícios
 
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
-  };
-};
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+// const section = document.querySelectorAll('#animais section');
 
-initAnimationScroll();
+// section.forEach((item) => {
+//   item.dataset.anime = 'show-down';
+// });
+
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+// Substitua todas as classes js- por data atributes.
